cc3c69a93c7d8ac7d9c0e8b797f49629f69a2f9f
renanmoura6941 <naneraruom@gmail.com>
2020-12-26 17:54:12 -0300

    md5

--- repository.js
+++ repository.js
@@ -1,12 +1,29 @@
-var url = "http://gateway.marvel.com/v1/public/characters?ts=1387249200000&apikey=11e884f682fd0f940f631c8682c94b65&hash=f1fcb5c90e9a89cea33e33f4442ae76b";//Sua URL
+import {PUBLIC_KEY,PRIVATE_KEY } from './config.js'
 
-var xhttp = new XMLHttpRequest();
-xhttp.open("GET", url, true);
-
-xhttp.onreadystatechange = function(){
-    if ( xhttp.readyState == 4 && xhttp.status == 200 ) {
-        console.log(xhttp.responseText);
-    }
+function getHeroes(){    
 }
 
-xhttp.send();
+function getTimeTemp(){
+  return  new Date().getTime(); 
+}
+
+function generateHash(){
+  return CryptoJS.MD5(getTimeTemp()+PUBLIC_KEY+PRIVATE_KEY)
+}
+
+
+getHeroes()
+
+
+//var url = "http://gateway.marvel.com/v1/public/characters?ts=1387249200000&apikey=11e884f682fd0f940f631c8682c94b65&hash=f1fcb5c90e9a89cea33e33f4442ae76b";//Sua URL
+
+// var xhttp = new XMLHttpRequest();
+// xhttp.open("GET", url, true);
+
+// xhttp.onreadystatechange = function(){
+//     if ( xhttp.readyState == 4 && xhttp.status == 200 ) {
+//         console.log(xhttp.responseText);
+//     }
+// }
+
+// xhttp.send();
