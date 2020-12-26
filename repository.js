98f916690568c8d7d958719415357cdeb3193061
renanmoura6941 <naneraruom@gmail.com>
2020-12-26 18:07:46 -0300

    code refactoring

--- repository.js
+++ repository.js
@@ -1,29 +1,20 @@
-import {PUBLIC_KEY,PRIVATE_KEY } from './config.js'
+import { PUBLIC_KEY, PRIVATE_KEY } from './config.js'
 
-function getHeroes(){    
+let generateHash = ()=> CryptoJS.MD5(getTimeTemp() + PUBLIC_KEY + PRIVATE_KEY);
+let getTimeTemp = () => new Date().getTime();
+
+function getHeroes() {
+  let url = `http://gateway.marvel.com/v1/public/characters?ts=${getTimeTemp()}&apikey=${PUBLIC_KEY}=${generateHash()}`;
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("GET", url, true);
+  xhttp.onreadystatechange = function () {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+      console.log(xhttp.responseText);
+    }
+  }
+  xhttp.send();
 }
-
-function getTimeTemp(){
-  return  new Date().getTime(); 
-}
-
-function generateHash(){
-  return CryptoJS.MD5(getTimeTemp()+PUBLIC_KEY+PRIVATE_KEY)
-}
-
 
 getHeroes()
 
 
-//var url = "http://gateway.marvel.com/v1/public/characters?ts=1387249200000&apikey=11e884f682fd0f940f631c8682c94b65&hash=f1fcb5c90e9a89cea33e33f4442ae76b";//Sua URL
-
-// var xhttp = new XMLHttpRequest();
-// xhttp.open("GET", url, true);
-
-// xhttp.onreadystatechange = function(){
-//     if ( xhttp.readyState == 4 && xhttp.status == 200 ) {
-//         console.log(xhttp.responseText);
-//     }
-// }
-
-// xhttp.send();
