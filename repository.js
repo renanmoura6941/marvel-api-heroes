7a563760d51e16f1347cfd7b583daa4e6171f4ce
renanmoura6941 <naneraruom@gmail.com>
2020-12-26 09:24:43 -0300

    project structure

---
+++ repository.js
@@ -0,0 +1,12 @@
+var url = "http://gateway.marvel.com/v1/public/characters?ts=1387249200000&apikey=11e884f682fd0f940f631c8682c94b65&hash=f1fcb5c90e9a89cea33e33f4442ae76b";//Sua URL
+
+var xhttp = new XMLHttpRequest();
+xhttp.open("GET", url, true);
+
+xhttp.onreadystatechange = function(){
+    if ( xhttp.readyState == 4 && xhttp.status == 200 ) {
+        console.log(xhttp.responseText);
+    }
+}
+
+xhttp.send();
