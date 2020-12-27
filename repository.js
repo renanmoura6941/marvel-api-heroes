33280f0601f1fcc4b16cf2a7beb1869c5723c062
renanmoura6941 <naneraruom@gmail.com>
2020-12-27 08:36:09 -0300

    automating get heros function

--- repository.js
+++ repository.js
@@ -1,17 +1,31 @@
 import { PUBLIC_KEY, PRIVATE_KEY } from './config.js'
 
-let generateHash = ()=> CryptoJS.MD5(getTimeTemp() + PUBLIC_KEY + PRIVATE_KEY);
-let getTimeTemp = () => new Date().getTime();
+var limit = 0;
+
+let generateHash = (timesTemp)=> CryptoJS.MD5(timesTemp + PRIVATE_KEY +PUBLIC_KEY );
+
+let getTimesTemp = () => new Date().getTime();
 
 function getHeroes() {
-  let url = `http://gateway.marvel.com/v1/public/characters?ts=${getTimeTemp()}&apikey=${PUBLIC_KEY}=${generateHash()}`;
+
+  var timesTemp = getTimesTemp();
+  
+  let url = `http://gateway.marvel.com/v1/public/characters?limit=${limit+10}&ts=${timesTemp}&apikey=${PUBLIC_KEY}&hash=${generateHash(timesTemp)}`;
+  
   var xhttp = new XMLHttpRequest();
+  
   xhttp.open("GET", url, true);
+  
   xhttp.onreadystatechange = function () {
+    
     if (xhttp.readyState == 4 && xhttp.status == 200) {
-      console.log(xhttp.responseText);
+    
+    var listHeroes = xhttp.responseText["data"]["results"];
+
     }
+    
   }
+  
   xhttp.send();
 }
 
